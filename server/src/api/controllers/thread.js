4cf8555c468e19790ddf69c47999e172cb4e5069
onlyzabao <ntgbao.235@gmail.com>
2023-07-29 11:39:32 +0700

    get thread by id

--- server/src/api/controllers/thread.js
+++ server/src/api/controllers/thread.js
@@ -1,6 +1,23 @@
-export const getThread = async (req, res) => {
+import Thread from "../models/thread"
+import Post from "../models/post"
 
-}
+export const getThread = async (req, res) => {
+    let rootID = req.param["id"];
+
+    const thread = await Thread.findById(rootID);
+
+    let children;
+    if (thread.type === "1") {
+        children = Thread.find({"rootID": rootID});
+    } else {
+        children = Post.find({"rootID": rootID});
+    }
+    
+    return res.status(200).json({
+        "thread": thread,
+        "children": children
+    });
+};
 
 export const createThread = async (req, res) => {
 
