f4c946fe85b7f9b41b188ebbf811341b02b22381
danhpb <21020289@vnu.edu.vn>
2023-07-29 14:59:15 +0700

    add main thread screen

--- client/uet-forum/src/components/main-thread/MainThread.js
+++ client/uet-forum/src/components/main-thread/MainThread.js
@@ -1,10 +1,32 @@
 import { useParams } from "react-router-dom"
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function MainThread() {
     const params = useParams();
+    const [childThreadList, setChildThreadList] = useState([]);
+    const [title, setTitle] = useState("");
+    useEffect(() => {
+        axios.get(`http://localhost:5050/api/thread/${params.threadID}`)
+            .then((res) => {
+                console.log(res.data);
+                setChildThreadList(res.data.children);
+                setTitle(res.data.thread.title)
+            })
+    })
+    
     return (
-        <h1>
-            MainThread {params.threadID}
-        </h1>
+        <>
+            <h1>{title}</h1>
+            <br/>
+        {childThreadList.map((childThread) =>
+            <>
+                <h2><Link to={`st/${childThread._id}`}>{childThread.title}</Link></h2>
+                <br />
+            </>
+            )}
+        </>
     )
 }
