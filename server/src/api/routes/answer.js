25ebcce61771b64ae36aeacd53ee4b56bb7872fd
onlyzabao <ntgbao.235@gmail.com>
2023-07-29 17:56:26 +0700

    fix get user

--- server/src/api/routes/answer.js
+++ server/src/api/routes/answer.js
@@ -1,19 +1,23 @@
 import {
     Router
 } from "express";
+
 import {
     deleteAnswer,
     getAnswer,
     updateAnswer
 } from "../controllers/answers";
+
 import {
     verifyToken
 } from "../middlewares/verify";
 
+import catchAsync from "../exceptions/catch-async";
+
 const answerRoute = new Router;
 
 answerRoute.get("/:id", getAnswer);
-answerRoute.put("/:id/edit", verifyToken, updateAnswer);
-answerRoute.delete("/:id/", verifyToken, deleteAnswer);
+answerRoute.put("/:id/edit", verifyToken, catchAsync(updateAnswer));
+answerRoute.delete("/:id/", verifyToken, catchAsync(deleteAnswer));
 
 export default answerRoute;
--- server/src/api/routes/user.js
+++ server/src/api/routes/user.js
@@ -21,7 +21,7 @@ import {
 const userRoute = Router();
 
 userRoute.get("/", verifyToken, catchAsync(getAllUsers));
-userRoute.get("/:id", catchAsync(getUser));
+userRoute.get("/view/:id", catchAsync(getUser));
 userRoute.post("/find", verifyToken, catchAsync(findUser));
 userRoute.post("/create", catchAsync(createUser));
 userRoute.get("/view", verifyToken, catchAsync(viewUser));
